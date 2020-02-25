098530ec200cef3cba7ac6cc809bd6e854f1407f
Peter Elliott <pelliott@ualberta.ca>
2020-02-25 13:14:48 -0700

    added the post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import Author
 
 # Create your models here.
+class Post(models.Model):
+    date = models.DateField()
+    title = models.CharField(max_length=150)
+    content = models.CharField(max_length=8192)
+    markdown = models.BooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,
+                               related_name='posts')
--- polarbear/settings.py
+++ polarbear/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
