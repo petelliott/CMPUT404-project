e59dfddaf59c406f8653c7faea4c27406836b9a9
Peter Elliott <pelliott@ualberta.ca>
2020-02-26 11:40:29 -0700

    added a view to see all posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('friend',views.friend,name='friend'),
     path('post',views.post,name='post'),
     path('post/<int:post_id>', views.viewpost, name='viewpost'),
+    path('posts/all', views.allposts, name='allposts'),
     path('profile',views.profile,name='profile'),
 ]
