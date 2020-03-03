0fd14f2a302be88a4cf881cf6d928d47f5221364
Peter Elliott <pelliott@ualberta.ca>
2020-03-03 13:16:10 -0700

    added privacy to post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from users.models import Author
 
+class Privacy:
+    #TODO: private to specific author
+    PRIVATE  = 0
+    URL_ONLY = 1
+    FRIENDS  = 2
+    FOAF     = 3
+    PUBLIC   = 4
+
 # Create your models here.
 class Post(models.Model):
     date = models.DateField()
@@ -9,3 +17,4 @@ class Post(models.Model):
     markdown = models.BooleanField(default=False)
     author = models.ForeignKey(Author, on_delete=models.CASCADE,
                                related_name='posts')
+    privacy = models.IntegerField(default=Privacy.PUBLIC)
