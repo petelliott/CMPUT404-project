890efe4ea996bb76e1830d9040da1a2737c1dc81
Peter Elliott <pelliott@ualberta.ca>
2020-03-06 12:15:46 -0700

    added a buch of empty views

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 
 urlpatterns = [
+    path('author/posts', views.author_posts, name="api_authorposts"),
+    path('posts', views.posts, name="api_posts"),
+    path('author/<int:author_id>/posts', views.authorid_posts,
+         name="api_authoridposts"),
+    path('posts/<int:post_id>', views.post, name="api_post"),
+    path('posts/<int:post_id>/comments', views.post_comments,
+         name="api_post_comments"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def author_posts(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def authorid_posts(request, author_id):
+    pass
+
+
+def post(request, post_id):
+    pass
+
+
+def post_comments(request, post_id):
+    pass
