dc27af9d88903ab572ec217ddb9b2fa5d2f74b72
Peter Elliott <pelliott@ualberta.ca>
2020-03-08 15:58:07 -0600

    added test for /posts/{POST_ID}

--- api/tests.py
+++ api/tests.py
@@ -101,3 +101,37 @@ class ViewAuthorTestCase(TestCase):
             "http://testserver/api/author/{}".format(self.author_A.pk),
             j["url"])
         self.assertEqual(2, len(j["friends"]))
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.author = Author.signup("author", "pw", "pw")
+        self.post = Post.objects.create(date=datetime.date.today(),
+                                        title="a",
+                                        content="a",
+                                        author=self.author,
+                                        privacy=Privacy.PUBLIC)
+
+    def test_post(self):
+        j = self.c.get("/api/posts/{}".format(self.post.pk)).json()
+
+        exp = self.post
+        act = j
+
+        self.assertEqual(exp.title, act["title"])
+        self.assertEqual("http://testserver/api/posts/{}".format(exp.pk),
+                         act["source"])
+        self.assertEqual("http://testserver/api/posts/{}".format(exp.pk),
+                         act["origin"])
+        self.assertEqual(exp.content_type, act["contentType"])
+        self.assertEqual(exp.content, act["content"])
+        self.assertEqual("http://testserver/api/author/{}".format(exp.author.pk),
+                         act["author"]["id"])
+        self.assertEqual("http://testserver/api/",
+                         act["author"]["host"])
+        self.assertEqual(exp.author.user.username, act["author"]["displayName"])
+        self.assertEqual("http://testserver/api/author/{}".format(exp.author.pk),
+                         act["author"]["url"])
+        self.assertEqual(exp.pk, act["id"])
+        self.assertEqual("PUBLIC", act["visibility"])
+        self.assertEqual(False, act["unlisted"])
