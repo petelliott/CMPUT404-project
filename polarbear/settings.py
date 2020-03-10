a3f2ff13e3ac1f465edc1c924af73376b16144d6
colinchoix <cechoi@ualberta.ca>
2020-03-10 11:32:53 -0600

    User css added to static folder

--- polarbear/settings.py
+++ polarbear/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    "/users/static/",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
